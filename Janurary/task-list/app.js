34d885c88413d3a8e939a4136090d4aed2093a16
joshuashaffer189 <jshaffer189@gmail.com>
2020-01-28 00:06:43 -0600

    mistaken commit

--- Janurary/task-list/app.js
+++ Janurary/task-list/app.js
@@ -1,30 +1,46 @@
 // TODO JS
 
 // array of active and inactive tasks
-const taskArray = [];
-const completedArray = [];
+const tasks = [ 'yes', 'np', 43, 'fdff' ];
 
 // select input element
 const task = document.querySelector('input');
 
 task.addEventListener('keyup', function(event) {
-	// Enter Key
 	if (event.keyCode === 13) {
 		pushTask(event);
 	}
 });
 
 // push array function
-const pushTask = (input) => {
-	taskArray.push(input.target.value);
-	console.log(taskArray);
+const pushTask = (event) => {
+	tasks.push(event.target.value);
+	console.log(tasks);
 	task.value = '';
 };
 
 // loop through an array and create unordered list
-const taskLoop = (array) => {
-	// loop through each array element
-	// append with a html element create
-	for (let item of array) {
+const renderTasks = (array) => {
+
+	if (array.length === 0) {
+		return `No Tasks yet!`
+	} else {
+		for (let j = 0; j > array.length; j++)
+		let li = document.createElement('li')
+		
+		
 	}
+
+
+
+	// let getList = document.getElementById(listId);
+	// return array.forEach(function(item) {
+	// 	let li = document.createElement('li');
+	// 	getList.append(li);
+	// 	li.innerHTML += item;
+	// });
 };
+
+// call array in to DOM
+renderTasks(tasks);
+
